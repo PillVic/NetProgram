20baa64c525d0fb22fce0161d79abcf07113d666
PillVic <benlmf@qq.com>
2018-12-22 09:54:46 +0800

    Exp 4 done

--- Exp4/GetWeather.py
+++ Exp4/GetWeather.py
@@ -1,45 +1,54 @@
-#!/usr/bin/env python3
-#-*- coding:utf-8 -*-
+rom urllib.request import urlopen
+from bs4 import BeautifulSoup
 
-'''
-使用http.client等库来获取天气信息
-'''
+city = dict()
+with open('dict.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        cityName, cityCode = line.split(':')
+        city[cityName] = cityCode.rstrip(' \n')
 
-import sys
-import requests
-import json
-import http.client
+def getWeather(cityName):
+    cityCode = city.get(cityName)
+    if cityCode == None:
+        return '[Error]: City Not Found'
+    print(cityCode)
+    resp=urlopen('http://www.weather.com.cn/weather/'+str(cityCode)+'.shtml')
+    soup=BeautifulSoup(resp,'html.parser')
 
-base = 'http://cdn.sojson.com/_city.json'
-r = requests.get(base)
-answer = r.json()
-Code=dict()
-for i in answer:
-    Code[i['city_name']] = i['city_code']
+    today = soup.find('p', class_='tem')
+    date=soup.find('ul',class_="t clearfix").h1.string
+    weather=soup.find('p', class_="wea").string
+    wind=soup.find('p',class_='win').i.string
+    low=today.i.string
+    high=today.find_next('p', class_="tem").span.string
+    ray=soup.find('li',class_='li1').p.string
+    sport=soup.find('li',class_='li2 hot').p.string
+    bloodsugar=soup.find('li',class_='li5').p.string
+    wear=soup.find('li',class_='li3 hot').p.string
+    washcar=soup.find('li',class_='li4').p.string
+    pollute=soup.find('li',class_='li6').p.string
 
-def getCode(city):
-    #解析json文件得到指定城市的编号并返回
-    return Code[city]
+    info = '''
+    天气:%s
+    风级:%s
+    最低气温:%s
+    最高气温:%s
+    紫外线指数:%s
+    减肥指数:%s
+    血糖指数:%s
+    穿衣指数:%s
+    洗车指数:%s
+    空气污染指数:%s
+    ''' %(weather, wind, low, high, ray, sport, bloodsugar, wear, washcar, pollute)
+    return info
 
-def getWeather(city):
-    code = getCode(city)
-    connection = http.client.HTTPConnection('t.weather.sojson.com')
-    connection.request('GET', '/api/weather/city/'+code)
-    rawreply = connection.getresponse().read()
-    reply = json.loads(rawreply.decode('utf-8'))
-    print(reply)
-    data = reply['data']
-    return str(data['forecast'])
-    return '湿度%s, 温度%s, 空气质量%s, %s' % \
-           (data['shidu'][:-1], data['wendu'], data['quality'], data['ganmao'])
+
+
+
 
 if __name__ == '__main__':
-    if len(sys.argv) >=2:
-        print(getWeather(sys.argv[1]))
-    else :
-        while True:
-            city = input('Please input the city:')
-            if city =='EOF':
-                break
-            print(getWeather(city))
+    while True:
+        cityName = input('Please input the cityName: ')
+        print(getWeather(cityName))
 
